165ec81a957c0b37603fb8c13a589e25ca1952fb
Manishv-Edviron <manish.verma@edviron.com>
2025-08-22 17:44:22 +0530

    initial commit

---
+++ task_2.py
@@ -0,0 +1,4 @@
+user_first_name = input('Enter your first name: ')
+user_last_name = input('Enter your last name: ')
+
+print(f'hello {user_first_name} {user_last_name}! Welcome to the python program.')
